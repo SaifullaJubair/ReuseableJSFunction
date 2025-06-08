b5ae30299dcd6e24d1521d85fd3487e0f9aed04f
SaifullaJubair <saifullajubair6@gmail.com>
2025-06-08 23:57:12 +0600

    Refactor date formatting functions for consistency and clarity

--- src/utils/dateFormate/DateFormate.js
+++ src/utils/dateFormate/DateFormate.js
@@ -1,4 +1,9 @@
 // Format date to 'DD-MM-YYYY'
 export const formatDate = (date) => {
-  return new Date(date).toLocaleDateString('en-GB'); // UK format: DD/MM/YYYY
+  return new Date(date).toLocaleDateString("en-GB"); // UK format: DD/MM/YYYY
+};
+
+// Format to ISO string without time
+export const toISODate = (date) => {
+  return new Date(date).toISOString().split("T")[0]; // 'YYYY-MM-DD'
 };
