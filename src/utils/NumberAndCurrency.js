f9c3033f57a12a5397789eb7f3e0545a31aea6e5
SaifullaJubair <saifullajubair6@gmail.com>
2025-06-11 21:37:11 +0600

    Add formatCurrency utility for formatting amounts with currency

--- src/utils/NumberAndCurrency.js
+++ src/utils/NumberAndCurrency.js
@@ -3,3 +3,10 @@ export const formatNumber = (num) => {
   return num.toLocaleString();
 };
 
+// Format currency
+export const formatCurrency = (amount, currency = "USD") => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(amount);
+};
